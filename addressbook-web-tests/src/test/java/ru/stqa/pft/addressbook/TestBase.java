d86bb863df4b3bfffb1013c09c26bfbdab75ec26
deathfuzzo <53620371+deathfuzzo@users.noreply.github.com>
2019-08-28 12:20:20 +0300

    AppManager and TestBase Edited
    Разбитие на архитектуры на слои

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -11,67 +11,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TestBase extends ApplicationManager {
-  JavascriptExecutor js;
-  protected WebDriver driver;
-  protected Map<String, Object> vars;
 
-  @BeforeMethod //метод инициализации фикстуры ("фиксации" тестового метода)
+    @BeforeMethod //метод инициализации фикстуры ("фиксации" тестового метода)
   public void setUp() {
     init();
   }
 
-  private void init() {
-    driver = new FirefoxDriver();
-    js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
-    driver.get("http://localhost/addressbook/");
-    login("admin", "secret");
-  }
-
-  protected void login(String username, String password) {
-    driver.findElement(By.name("user")).sendKeys(username);
-    driver.findElement(By.name("pass")).sendKeys(password);
-    driver.findElement(By.xpath("//form[@id=\'LoginForm\']/input[3]")).click();
-  }
-
-  protected void returnToGroupPage() {
-    driver.findElement(By.linkText("group page")).click();
-  }
-
-  protected void submitGroupCreation() {
-    driver.findElement(By.name("submit")).click();
-  }
-
-  protected void fillGroupForm(GroupData groupData) {
-    driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    driver.findElement(By.name("group_header")).click();
-    driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  protected void initGroupCreation() {
-    driver.findElement(By.xpath("//div[@id=\'content\']/form/input[4]")).click();
-  }
-
-  protected void gotoGroupPage() {
-    driver.findElement(By.linkText("groups")).click();
-  }
-
-  @AfterMethod //метод завершения фикстуры ("фиксации" тестового метода)
+    @AfterMethod //метод завершения фикстуры ("фиксации" тестового метода)
   public void tearDown() {
     stop();
   }
 
-  private void stop() {
-    driver.quit();
-  }
-
-  protected void deleteGroup() {
-    driver.findElement(By.xpath("(//input[@name=\'delete\'])[2]")).click();
-  }
-
-  protected void selectGroup() {
-    driver.findElement(By.xpath("(//input[@name=\'selected[]\'])[15]")).click();
-  }
 }
